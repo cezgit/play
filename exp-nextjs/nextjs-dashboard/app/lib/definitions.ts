209c511d1c2f359a86ecea9f68af50eb0233230d
Cezar <cezgit@gmail.com>
2024-05-18 22:17:40 -0700

    fetch more data

--- exp-nextjs/nextjs-dashboard/app/lib/definitions.ts
+++ exp-nextjs/nextjs-dashboard/app/lib/definitions.ts
@@ -10,14 +10,14 @@ export type User = {
   name: string;
   email: string;
   password: string;
-};
+} & QueryResultRow;
 
 export type Customer = {
   id: string;
   name: string;
   email: string;
   image_url: string;
-};
+} & QueryResultRow;
 
 export type Invoice = {
   id: string;
@@ -27,12 +27,12 @@ export type Invoice = {
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
-};
+} & QueryResultRow;
 
 export type Revenue = {
   month: string;
   revenue: number;
-} & QueryResultRow;
+};
 
 export type LatestInvoice = {
   id: string;
@@ -56,7 +56,7 @@ export type InvoicesTable = {
   date: string;
   amount: number;
   status: 'pending' | 'paid';
-};
+} & QueryResultRow;
 
 export type CustomersTableType = {
   id: string;
@@ -66,7 +66,7 @@ export type CustomersTableType = {
   total_invoices: number;
   total_pending: number;
   total_paid: number;
-};
+} & QueryResultRow;
 
 export type FormattedCustomersTable = {
   id: string;
@@ -76,12 +76,12 @@ export type FormattedCustomersTable = {
   total_invoices: number;
   total_pending: string;
   total_paid: string;
-};
+} & QueryResultRow;
 
 export type CustomerField = {
   id: string;
   name: string;
-};
+} & QueryResultRow;
 
 export type InvoiceForm = {
   id: string;
